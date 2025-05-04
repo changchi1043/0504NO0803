1d65dbf430559c2058a1f00f0413c5d875c6913c
changchi1043 <410001043@gms.tku.edu.tw>
2025-05-04 23:17:23 +0800

    final project

---
+++ app/src/main/java/com/example/a803/Contact.kt
@@ -0,0 +1,8 @@
+package com.example.a803
+
+data class Contact(
+    val name: String,
+    // 電話
+    val number: String
+
+)
---
+++ app/src/main/java/com/example/a803/SecActivity.kt
@@ -0,0 +1,51 @@
+package com.example.a803
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sec)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val edName = findViewById<EditText>(R.id.edName)
+        val eedNumber = findViewById<EditText>(R.id.edNumber)
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        // 設定按鈕監聽器，取得輸入的姓名與電話
+        btnSend.setOnClickListener {
+            //判斷是否輸入資料
+            when {
+                edName.text.isEmpty() -> showToast("請輸入姓名")
+
+                eedNumber.text.isEmpty() -> showToast("請輸入編號")
+
+                else -> {
+                    val b = Bundle()
+                    b.putString("name", edName.text.toString())
+                    b.putString("phone", eedNumber.text.toString())
+                    //使用setResult()回傳聯絡人資料
+                    setResult(Activity.RESULT_OK, Intent().putExtras(b))
+                    finish()
+                }
+            }
+        }
+    }
+
+    // 建立 showToast 方法顯示 Toast 訊息
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+}
